622c7f083ffc587b468bfdef42ab76cb23741aec
strdr4605 <str.dr4605@gmail.com>
2018-07-14 17:53:55 +0200

    Close #46 new svg logos, add css and js that create a real-time clock.

---
+++ js/svg.js
@@ -0,0 +1,9 @@
+let curentHour = new Date().getHours() % 12;
+let curentMinute = new Date().getMinutes();
+let hour = document.getElementsByClassName('hour')[0];
+let minute = document.getElementsByClassName('minute')[0];
+let offset = 60; // initialy watch arrows are on 2 o'clock.
+let degHour = 30;
+let degMinute = 6;
+hour.style.transform = `rotate(${curentHour * degHour - offset}deg)`;
+minute.style.transform = `rotate(${curentMinute * degMinute - offset}deg)`;
