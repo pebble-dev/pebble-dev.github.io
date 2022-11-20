94c30c04412b659b594c9608c89896523d3eeefd
Willow Systems <mail@willow.systems>
2022-11-20 16:29:22 +0000

    Extend hackathon end date to 23:59:59 on Sunday GMT

--- js/hackathon.js
+++ js/hackathon.js
@@ -1,9 +1,9 @@
 
 hackathon_start_date = new Date("2022-11-18T18:00:00Z")
-hackathon_end_date = new Date("2022-11-20T18:00:00Z")
+hackathon_end_date = new Date("2022-11-20T23:59:59Z")
 
-//hackathon_start_date = new Date("2022-11-16T22:19:30Z")
-//hackathon_end_date = new Date("2022-11-16T22:20:30Z")
+//hackathon_start_date = new Date("2022-11-18T18:14:00Z")
+//hackathon_end_date = new Date("2022-11-18T18:16:00Z")
 
 in_progress_link_href = "https://rebble.io/discord"
 confetti_already = false
